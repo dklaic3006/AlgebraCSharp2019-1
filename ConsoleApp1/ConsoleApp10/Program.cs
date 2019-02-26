9e4e44d957f3d848a530d89b1a99b20416851b0c
dklaic3006 <dklaic3006@outlook.com>
2019-02-26 14:42:13 +0100

    dodani zadaci 5.2 zbirka

---
+++ ConsoleApp1/ConsoleApp10/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0, b = 0;
+            string c = "";
+
+            Console.Write("Unesite 1. broj: ");
+            a = float.Parse(Console.ReadLine());
+
+            Console.Write("Unesite 2. broj: ");
+            b = float.Parse(Console.ReadLine());
+
+            Console.Write("Unesite željenu računsku operaciju (+, -, *, /): ");
+            c = Console.ReadLine();
+            
+            switch (c)
+            
+            {
+                case "+":
+                case "plus":
+
+                    Console.WriteLine("Zbroj je: {0} + {1} = {2} ", a, b, (a + b));
+                    break;
+
+                case "-":
+                    Console.WriteLine("Razlika je: {0} - {1} = {2} ", a, b, (a - b));
+                    break;
+
+                case "*":
+                case "x":
+                    Console.WriteLine("Umnožak je: {0} * {1} = {2} ", a, b, (a * b));
+                    break;
+
+                case "/":
+                    Console.WriteLine("Kvocijent je: {0} / {1} = {2} ", a, b, (a / b));
+                    break;
+            }
+
+            Console.Write("\n Za novu računsku operacuju unesite \"Y\" \n Za kraj pritisnite ENTER: " );
+            string y = Console.ReadLine();
+
+            //if (y == "Y" || y == "y")
+
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._2._5_u_intervalu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float broj = 0;
+            Console.Write("Unesite jedan broj: ");
+            broj = int.Parse(Console.ReadLine());
+
+            if (broj > 100 && broj < 200)
+            {
+                Console.WriteLine(" Unešeni broj je u intervalu 100 do 200");
+            }
+
+           
+            else
+            {
+                Console.WriteLine(" Unešeni broj nije u intervalu 100 do 200");
+            }
+
+            Console.WriteLine("\n\tZa kraj pritisnite ENTER!");
+            Console.ReadLine();
+        }
+    }
+}
