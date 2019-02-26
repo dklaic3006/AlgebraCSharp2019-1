1a18fc0903ba0b3e44094622ba1184b98bdac927
dklaic3006 <dklaic3006@outlook.com>
2019-02-26 18:24:22 +0100

    dadana zbirka 5.3.1

---
+++ ConsoleApp1/ConsoleApp14/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petlje_primjeri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nispis svih brojeva 0,1,2......99!");
+            //ispis svih brojeva 0,1,2......99
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("\n\nispis svih brojeva 16,18......89!");
+            //ispis svih brojeva 16,18......89
+            for (int i = 16; i < 89; i+=2)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("\n\nispis svih brojeva 135,130,125.....55!");
+            //ispis svih brojeva 135,130,125.....55
+            for (int i = 135; i > 54; i -= 5)
+            {
+                Console.Write("{0} ", i);
+            }
+            
+            Console.WriteLine("\n\nispis svih brojeva od 0 do 1000 djeljivih sa 17!");
+            //ispis svih brojeva 01,1,2....99 preskoči 80
+            for (int i = 0; i > 10000; i++)
+            {
+                if ( i % 17 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+                
+            }
+
+            Console.WriteLine("\n\nispis svih brojeva 135,130,125.....55!");
+            //ispis svih brojeva 01,1,2....99 preskoči 80
+            for (int i = 135; i > 54; i -= 5)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("\n\ntablica množenja");
+            
+            for (int i = 1; i <= 10; i++)
+
+            {
+                for (int j = 0; j <=10; j++)
+                {
+                Console.Write("{0}\t ",(i*j), i);
+                }
+                
+            }
+
+            Console.WriteLine("\n\n\nPritisnite bilo koju tipku za kraj!");
+
+            Console.ReadKey();
+        }
+    }
+}
