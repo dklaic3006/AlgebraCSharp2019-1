a8337f59cd69af5febbf6d647cf6d14d3089df72
dklaic3006 <dklaic3006@outlook.com>
2019-03-04 10:48:34 +0100

    dodan 5.3.18 unos ocjene

---
+++ ConsoleApp1/ConsoleApp17/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_2._3._18_unos_ocjene
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            bool unos = false;
+            int ocjena = 0;
+            while(!unos)
+
+            //while (!(ocjena > 1) && (ocjena < 5))
+            {
+                Console.Write("Unos ocjene: ");
+                ocjena = int.Parse(Console.ReadLine());
+                
+                if ((ocjena >= 1) && (ocjena <= 5))
+                {
+                    unos = true;
+                    
+                    Console.WriteLine("\nUnijeli ocjenu {0}", ocjena);
+                    Console.WriteLine("Unos ocjene obavljen");
+                }
+                else
+                {
+                    Console.WriteLine("Pogrešan unos, unijeli ste ocjenu: {0}", ocjena);
+                    Console.Write("\nMolim ponovite ");
+                }
+
+            }
+            {
+            Console.WriteLine("\nstisni bilo koju tipku za kraj");
+            Console.ReadLine();
+            }
+
+
+        }
+    }
+}
