79f4c0cd479afae653d5ccd8f706cc76111f517b
dklaic3006 <dklaic3006@outlook.com>
2019-03-06 00:57:07 +0100

    729

---
+++ ConsoleApp1/Zbirka 7.2.9 Prosjek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbirka_7._2._9_Prosjek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
